c07b794c12048f44f2ad59b7d4e23c71e9a540fb
PrathamSoni23 <pratham2374@gmail.com>
2024-09-12 14:39:43 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pratham-Soni-Learning-Angular';
+  course:string = 'Javascript Frameworks';
+  courseCredit:number = 3;
 }
