0910972094947eeb29b083a2b794374203405392
“Pratham <W0849814@myscc.ca>
2024-10-04 19:07:18 -0400

    Moved out the movieItems array ou tof movie-list.component.ts to mockMovie.ts Created a new service file called movies-data.service.ts

---
+++ src/app/data/mockMovie.ts
@@ -0,0 +1,13 @@
+
+import {Movie} from "../Shared/Models/movies";
+
+
+
+export const movieItems:Movie[] = [
+
+  {movieTitle: "Jumanji", director:"Yash", production: "YRF", yearReleased: 2019},
+  {movieTitle: "Black Panther", director:"Stan lee", production: "MCU", yearReleased: 2018},
+  {movieTitle: "Mission Impossible 10", director:"Dhwayne Johnson", production: "Rock Productions", yearReleased: 2002},
+  {movieTitle: "Smile", director:"Parker", production: "Temple Hill", yearReleased: 2022}
+
+];
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -14,16 +14,10 @@ import {Movies} from "../movies";
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
+
 export class MovieListComponent {
 
-  movieItems:Movie[] = [
 
-    {movieTitle: "Jumanji", director:"Yash", production: "YRF", yearReleased: 2019},
-    {movieTitle: "Black Panther", director:"Stan lee", production: "MCU", yearReleased: 2018},
-    {movieTitle: "Mission Impossible 10", director:"Dhwayne Johnson", production: "Rock Productions", yearReleased: 2002},
-    {movieTitle: "Smile", director:"Parker", production: "Temple Hill", yearReleased: 2022}
-
-  ];
 
 
 }
