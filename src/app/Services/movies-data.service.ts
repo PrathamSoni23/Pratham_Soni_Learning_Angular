9575f281e9b17c2ebc5b789e26b18224bfa8cf54
“Pratham <W0849814@myscc.ca>
2024-10-04 22:59:34 -0400

    added 4 new mwthods to add, find,, update and remove movies from the array.

--- src/app/Services/movies-data.service.ts
+++ src/app/Services/movies-data.service.ts
@@ -6,13 +6,47 @@ import {movieItems} from "../data/mockMovie";
 @Injectable({
   providedIn: 'root'
 })
+
 export class MoviesDataService {
+
+  private movies: Movie[] =movieItems;
 
   constructor() { }
   getMovies(): Observable<Movie[]> {
-
     return of(movieItems);
+
+  }
+
+  findMovieYear(releaseYear : number): Observable<Movie | undefined> {
+
+    const movieToFind = this.movies.find(user => user.yearReleased === releaseYear);
+    return of(movieToFind);
+
+  }
+
+  addMovie(newMovie : Movie): Observable<Movie[]> {
+
+    this.movies.push(newMovie)
+    return of(this.movies);
+
+  }
+
+  updateMovie(modifyMovie : Movie): Observable<Movie[]> {
+
+    const movieyear = this.movies.findIndex(user => user.yearReleased === modifyMovie.yearReleased);
+    if (movieyear !== -1) {
+      this.movies[movieyear] = modifyMovie;
+    }
+    return of(this.movies);
+
+  }
+
+  removeMovie(deleteMovie : number): Observable<Movie[]> {
+
+    this.movies = this.movies.filter(user => user.yearReleased !== deleteMovie);
+    return of(this.movies);
 
 
   }
+
 }
